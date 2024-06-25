4a6b8c278be3023fa63cf3118af543b1bf904afb
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2024-06-25 10:31:44 +0200

    removed not-existing calls

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
-import Image from "next/image";
+
 
 export default async function Home() {
-  const users = await fetch("http://localhost:3000/api/users");
-  const data = await users.json();
-console.log('data', data);
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      ass
